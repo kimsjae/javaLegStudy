6f952223fcc8462d9940c4db0dc5353726e33e9b
kimseongjae <stwin755@gmail.com>
2023-12-14 12:23:49 +0900

    ch3 예시문제

---
+++ src/ex02/Programming01.java
@@ -0,0 +1,18 @@
+package ex02;
+
+import java.util.Scanner;
+
+public class Programming01 {
+    public static void main(String[] args) {
+        /**
+         * 하나의 상자에 오렌지를 10개씩 담을 수 있다고 하자.
+         * 오렌지가 127개 있다면 상자 몇 개가 필요한가?
+         * 또 몇개의 오렌지가 남을까?
+         */
+
+        System.out.print("오렌지의 개수를 입력하시오: ");
+        Scanner sc = new Scanner(System.in);
+
+        sc.nextInt();
+    }
+}
