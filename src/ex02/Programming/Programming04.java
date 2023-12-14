c25e82245175f3ecb7d7ecbaeb95d798a0df39d2
kimseongjae <stwin755@gmail.com>
2023-12-14 18:04:22 +0900

    ch02.Programming04.수정01

--- src/ex02/Programming/Programming04.java
+++ src/ex02/Programming/Programming04.java
@@ -6,42 +6,42 @@ public class Programming04 {
          * 자바의 기본 논리 연산에 대한 진리표를 표시하는 프로그램을 작성해보자.
          */
 
-        boolean P, Q, AND, OR, XOR;
+        boolean p, q, and, or, xor;
         System.out.println("P\t\tQ\t\tAND\t\tOR\t\tXOR");
 
 
         // P와 Q가 모두 TRUE일 때
-        P = true;
-        Q = true;
-        AND = P && Q;
-        OR = P || Q;
-        XOR = (P != Q)? true : false;
-        System.out.println(P + "\t" + Q + "\t" + AND + "\t" + OR + "\t" + XOR);
+        p = true;
+        q = true;
+        and = p && q;
+        or = p || q;
+        xor = (p != q)? true : false;
+        System.out.println(p + "\t" + q + "\t" + and + "\t" + or + "\t" + xor);
 
 
         // P가 TRUE, Q가 FALSE일 때
-        P = true;
-        Q = false;
-        AND = P && Q;
-        OR = P || Q;
-        XOR = (P != Q)? true : false;
-      System.out.println(P + "\t" + Q + "\t" + AND + "\t" + OR + "\t" + XOR);
+        p = true;
+        q = false;
+        and = p && q;
+        or = p || q;
+        xor = (p != q)? true : false;
+      System.out.println(p + "\t" + q + "\t" + and + "\t" + or + "\t" + xor);
 
       // P가 FALSE, Q가 TRUE일 때
-      P = false;
-      Q = true;
-        AND = P && Q;
-        OR = P || Q;
-        XOR = (P != Q)? true : false;
-      System.out.println(P + "\t" + Q + "\t" + AND + "\t" + OR + "\t" + XOR);
+      p = false;
+      q = true;
+        and = p && q;
+        or = p || q;
+        xor = (p != q)? true : false;
+      System.out.println(p + "\t" + q + "\t" + and + "\t" + or + "\t" + xor);
 
       // P와 Q가 모두 FALSE일 때
-      P = false;
-      Q = false;
-        AND = P && Q;
-        OR = P || Q;
-        XOR = (P != Q)? true : false;
-        System.out.println(P + "\t" + Q + "\t" + AND + "\t" + OR + "\t" + XOR);
+      p = false;
+      q = false;
+        and = p && q;
+        or = p || q;
+        xor = (p != q)? true : false;
+        System.out.println(p + "\t" + q + "\t" + and + "\t" + or + "\t" + xor);
 
     }
 }
