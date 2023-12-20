d127dcb34554d9318a9b0d96f99bab0373afb35f
kimseongjae <stwin755@gmail.com>
2023-12-20 09:27:55 +0900

    Scope plus

--- src/ex04/test/ScopeEx01.java
+++ src/ex04/test/ScopeEx01.java
@@ -2,6 +2,14 @@ package ex04.test;
 
 public class ScopeEx01 {
 
+    static void m3() {
+        int u = 100;
+    }
+
+    void m4() {
+        System.out.println();
+    }
+
     int n1 = 1;
     static int n2 = 2;
 
@@ -22,5 +30,21 @@ public class ScopeEx01 {
         ScopeEx01 sc = new ScopeEx01(); // sc라는 변수는 메인 스택에, 메인에 sc는 heap의 주소가 담겨있다, 참조변수 ex) *50000
         System.out.println(sc.n1);
         sc.m2();
+
+        /**
+         * static (n2=2, m1(), main) -> 클래스명.x
+         * -> 어디에서든지 접근 가능하다.
+         *
+         * heap (n1=1, m2())
+         * -> heap끼리는 같은 scope 여서 접근이 가능하다.
+         *
+         * stack (n5-10)
+         * -> 외부 접근 불가능
+         *
+         * 데이터는 스택에 저장
+         * 실행은 스택 x
+         */
+
+        }
     }
 }
