5c7cecbc42999c17fb7db358edcf1d63e976ff41
kimseongjae <stwin755@gmail.com>
2023-12-20 10:01:16 +0900

    DeskLampClass

---
+++ src/ex04/DeskLamp.java
@@ -0,0 +1,19 @@
+package ex04;
+
+public class DeskLamp {
+    // 인스턴스 변수 정의
+    private boolean isOn; // 켜짐이나 꺼짐과 같은 램프의 상태
+
+    // 메소드 정의
+    public void turnOn() {
+        isOn = true;
+    }
+
+    public void turnOff() {
+        isOn = false;
+    }
+
+    public String toString() {
+        return "현재 상태는 " + (isOn == true ? "켜짐" : "꺼짐");
+    }
+}
