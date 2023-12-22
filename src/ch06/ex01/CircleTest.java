160a579b067cfb99ba1804827f6bb9a9dd4bb384
kimseongjae <stwin755@gmail.com>
2023-12-22 09:55:07 +0900

    ch06 ex01

---
+++ src/ch06/ex01/CircleTest.java
@@ -0,0 +1,31 @@
+package ch06.ex01;
+
+class Shape {
+    int x, y;
+
+    public Shape() {
+        System.out.println("Shape 생성됨");
+    }
+}
+
+class Circle extends Shape {
+    int radius; // 반지름
+
+    public Circle(int radius) {
+        System.out.println("Circle 생성됨");
+        this.radius = radius;
+        super.x = 0;
+        super.y = 0;
+    }
+
+    double getArea() {
+        return 3.14 * radius * radius;
+    }
+}
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10);
+        circle.getArea();
+    }
+}
