0e069bdb8467a59c3bc4fcaf44a252167fff7a80
kimseongjae <stwin755@gmail.com>
2023-12-22 14:02:43 +0900

    starcraft

---
+++ src/ch06/example4/Dragoon.java
@@ -0,0 +1,13 @@
+package ch06.example4;
+
+public class Dragoon extends Protoss {
+    String name;
+
+    public Dragoon(String name) {
+        this.name = name;
+    }
+
+    public void attack() {
+        System.out.println("드라군이 공격합니다." + name);
+    }
+}
---
+++ src/ch06/example4/Protoss.java
@@ -0,0 +1,7 @@
+package ch06.example4;
+
+public class Protoss {
+    public void attack() {
+
+    }
+}
