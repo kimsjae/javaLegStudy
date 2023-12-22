897babce8ad2e472f42281c448d42640ed69b358
kimseongjae <stwin755@gmail.com>
2023-12-22 17:37:21 +0900

    ch06 Programming02

---
+++ src/ch06/Programming/Programming02.java
@@ -0,0 +1,26 @@
+package ch06.Programming;
+
+class Animal {
+    void walk() {
+        System.out.println("걸을 수 있음");
+    }
+}
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("날을 수 있음");
+    }
+
+    void sing() {
+        System.out.println("노래 부를 수 있음");
+    }
+}
+
+public class Programming02 {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
