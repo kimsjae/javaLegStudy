ee9eddfe468239c6383a75a9a003d2ed8d2c2f3c
kimseongjae <stwin755@gmail.com>
2023-12-26 09:41:47 +0900

    ch06 Programming03

---
+++ src/ch06/Programming/Programming03.java
@@ -0,0 +1,29 @@
+package ch06.Programming;
+
+class Sports {
+    String getName() {
+        return "아직 결정되지 않음";
+    }
+
+    int getPlayers() {
+        return 0;
+    }
+}
+
+class Soccer extends Sports {
+    String getName() {
+        return "축구";
+    }
+
+    int getPlayers() {
+        return 11;
+    }
+}
+
+public class Programming03 {
+    public static void main(String[] args) {
+        Soccer s = new Soccer();
+        System.out.println("경기이름 : " + s.getName());
+        System.out.println("경기자수 : " + s.getPlayers());
+    }
+}
