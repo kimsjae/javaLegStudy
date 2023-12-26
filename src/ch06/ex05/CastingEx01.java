5da6e900a60a6e9321215d9539eb4417a9c88df8
kimseongjae <stwin755@gmail.com>
2023-12-26 10:45:11 +0900

    instanceof 연산자 배우기

--- src/ch06/ex05/CastingEx01.java
+++ src/ch06/ex05/CastingEx01.java
@@ -1,2 +1,36 @@
-package ch06.ex05;public class CastingEx01 {
+package ch06.ex05;
+
+class Protoss {
+
+}
+
+class Zealot extends Protoss {
+    public void attack() {
+        System.out.println("질럿이 공격합니다.");
+    }
+}
+
+class Dragoon extends Protoss {
+    public void attack() {
+        System.out.println("드라군이 공격합니다.");
+    }
+}
+
+public class CastingEx01 {
+    public static void start(Protoss p) {
+        if (p instanceof Zealot) {
+            Zealot u = (Zealot) p; // 부모를 자식에게 대입할 수 없다. 반대는 가능.
+            u.attack();
+        }
+
+        if (p instanceof Dragoon) {
+            Dragoon u = (Dragoon) p; // 다운캐스팅은 명시적으로
+            u.attack();
+        }
+    }
+
+    public static void main(String[] args) {
+        start(new Zealot());
+        start(new Dragoon());
+    }
 }
