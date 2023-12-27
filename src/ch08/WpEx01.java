8194d692fe482817f26b2da32acef37b79cdea27
kimseongjae <stwin755@gmail.com>
2023-12-27 18:37:55 +0900

    String문제 3개 풀기 전

---
+++ src/ch08/WpEx01.java
@@ -0,0 +1,12 @@
+package ch08;
+
+public class WpEx01 {
+    public static void main(String[] args) {
+        int num = 10;
+        Integer n = 10;
+
+        double d2 = n.doubleValue();
+
+        System.out.println(n.toString());
+    }
+}
